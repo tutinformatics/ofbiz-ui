59233d0a012048cb2c94ce75816cc59323727434
aroosi <aroosi@ttu.ee>
2020-02-24 12:24:54 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+export class App {
+    constructor() {
+      this.heading = 'Todos';
+      this.todos = [];
+      this.todoDescription = '';
+    }
+
+    addTodo() {
+      if (this.todoDescription) {
+        this.todos.push({
+          description: this.todoDescription,
+          done: false
+        });
+        this.todoDescription = '';
+      }
+    }
+
+    removeTodo(todo) {
+      let index = this.todos.indexOf(todo);
+      if (index !== -1) {
+        this.todos.splice(index, 1);
+      }
+    }
+  }
