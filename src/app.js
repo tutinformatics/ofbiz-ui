f6e675f4b411d7614d2aabb5ce56f6f2a2631f5d
karljoosepkarp <karljk@ttu.ee>
2020-03-15 17:59:59 +0200

    objektide-levi forms

--- src/app.js
+++ src/app.js
@@ -5,10 +5,12 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Workspace';
     config.map([
-      { route: '', moduleId: PLATFORM.moduleName('workspace/no-selection'),   title: 'Select'},
-      { route: 'workspace/:id',  moduleId: PLATFORM.moduleName('workspace/workspace-detail'), name:'contacts' },
-      { route: 'workspaces',  moduleId: PLATFORM.moduleName('workspace/workspaces'), name:'workspaces' },
-      { route: 'kanban',  moduleId: PLATFORM.moduleName('commons/kanban/kanban'), name:'kanban' }
+      {route: '', moduleId: PLATFORM.moduleName('workspace/no-selection'), title: 'Select'},
+      {route: 'workspace/:id', moduleId: PLATFORM.moduleName('workspace/workspace-detail'), name: 'contacts'},
+      {route: 'workspaces', moduleId: PLATFORM.moduleName('workspace/workspaces'), name: 'workspaces'},
+      {route: 'kanban', moduleId: PLATFORM.moduleName('commons/kanban/kanban'), name: 'kanban'},
+      {route: 'publisher', moduleId: PLATFORM.moduleName('objektide_levi/publisher/publisher'), name: 'publisher'},
+      {route: 'subscriber', moduleId: PLATFORM.moduleName('objektide_levi/subscriber/subscriber'), name: 'subscriber'}
     ]);
 
     this.router = router;
