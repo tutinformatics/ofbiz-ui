cdd6cde0210862d10d7c40d8721e8ba01f86ca7a
Raul <raaltm@ttu.ee>
2020-03-15 20:56:38 +0200

    Object distribution subscriber and publisher tabs

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ export class App {
       {route: 'workspace/:id', moduleId: PLATFORM.moduleName('workspace/workspace-detail'), name: 'contacts'},
       {route: 'workspaces', moduleId: PLATFORM.moduleName('workspace/workspaces'), name: 'workspaces'},
       {route: 'kanban', moduleId: PLATFORM.moduleName('commons/kanban/kanban'), name: 'kanban'},
-      {route: 'publisher', moduleId: PLATFORM.moduleName('objektide_levi/publisher/publisher'), name: 'publisher'},
+      {route: 'object-dist/publisher', moduleId: PLATFORM.moduleName('objektide_levi/publisher/publisher'), name: 'publisher'},
       {route: 'subscriber', moduleId: PLATFORM.moduleName('objektide_levi/subscriber/subscriber'), name: 'subscriber'},
       {route: 'object-dist', moduleId: PLATFORM.moduleName('objektide_levi/object-dist/object-dist'), name: 'object-dist'}
     ]);
