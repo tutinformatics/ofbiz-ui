d7918ef96237b64b859ac25d4f621e024924b894
aroosi <aroosi@ttu.ee>
2020-03-16 15:26:10 +0200

    Icons working more or less, no animations

--- src/commons/shuffle/shuffle.js
+++ src/commons/shuffle/shuffle.js
@@ -3,47 +3,10 @@ import {EventAggregator} from 'aurelia-event-aggregator';
 
 @inject(EventAggregator)
 export class Shuffle {
+
   stringArray = ['crm', 'accounting', 'calendar', 'contacts'];
 
+  assetPath = "/icons/" //"/src/resources/icons/";
+  svgFile = ".svg";
 
-
-
-
-
-  objArray = [
-    {name: 'Abi'},
-    {name: 'Bob'},
-    {name: 'Cherry'}
-  ];
-
-  fruits = ['Apple', 'Pineapple', 'Banana'];
-  animals = ['Cow', 'Horse', 'Sheep', 'Goat'];
-  insects = ['Grasshopper', 'Fly', 'Dragonfly', 'Frog'];
-
-  objArrayReordered(objArray, change) {
-    /* eslint no-console: 0 */
-    console.log(JSON.stringify(objArray));
-    console.log('change', change);
-  }
-
-  fruitsOrdered(list, change) {
-    console.log('newFruits: ' + list);
-    console.log('change', change);
-  }
-
-  animalsOrdered(list, change) {
-    console.log('animals: ' + list);
-    console.log('change', change);
-  }
-
-  insectsOrdered(list, change) {
-    console.log('insects: ' + list);
-    console.log('change', change);
-  }
-
-  constructor(ea) {
-    ea.subscribe('reorderable-group:intention-changed', intention => {
-      this.intention = intention;
-    });
-  }
 }
