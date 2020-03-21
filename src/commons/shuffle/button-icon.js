dba90466cec1decd6c05de519e4bc2098dbc3902
aroosi <aroosi@ttu.ee>
2020-03-21 13:26:40 +0200

    Add more or less working app menu

---
+++ src/commons/shuffle/button-icon.js
@@ -0,0 +1,63 @@
+import {inject, customElement, bindable} from 'aurelia-framework';
+import {EventAggregator} from 'aurelia-event-aggregator';
+import {autoinject} from "aurelia-dependency-injection";
+
+@inject(EventAggregator)
+@inject(Element)
+@customElement('button-icon')
+export class ButtonIcon {
+  @bindable elemName = '';
+  stringArray = ['crm', 'accounting', 'calendar', 'contacts'];
+
+  assetPath = "/icons/";
+  svgFile = ".svg";
+  active = "";
+  namething = '';
+
+
+
+  constructor() {
+    this.element = {
+          active: ''
+        };
+  }
+  x;
+  mouseOver() {
+    this.element.active = '-active';
+    //this.x = document.getElementById('shuffle-menu').querySelectorAll("button-icon");
+    //console.log(document.getElementById('shuffle-menu').querySelectorAll("button-icon"));
+    //this.x.style.width = 100;
+    /*var event = new CustomEvent('mouseover', {
+      detail: this.active,
+      bubbles: true
+    });
+
+    this.element.dispatchEvent(event);*/
+  }
+
+  mouseOut() {
+    this.element.active = '';
+  }
+  /*iconArray = {
+    [
+      {name:"cmr"},
+      {path:this.assetPath + "cmr" + this.iconArray.cmr.active + this.svgFile},
+      {active:""}
+    ],
+    [
+      {name:"accounting"},
+      {path:this.assetPath + "accounting" + this.iconArray.cmr.active + this.svgFile},
+      {active:""}
+    ],
+    [
+      {name:"calendar"},
+      {path:this.assetPath + "calendar" + this.iconArray.cmr.active + this.svgFile},
+      {active:""}
+    ],
+    [
+      {name:"contacts"},
+      {path:this.assetPath + "contacts" + this.iconArray.cmr.active + this.svgFile},
+      {active:""}
+    ]
+};*/
+}
--- src/commons/shuffle/shuffle.js
+++ src/commons/shuffle/shuffle.js
@@ -1,14 +1,19 @@
-import {inject} from 'aurelia-framework';
+import {inject, customElement, bindable} from 'aurelia-framework';
 import {EventAggregator} from 'aurelia-event-aggregator';
 
 @inject(EventAggregator)
 export class Shuffle {
-
   stringArray = ['crm', 'accounting', 'calendar', 'contacts'];
-
   assetPath = "/icons/" //"/src/resources/icons/";
   svgFile = ".svg";
-  active = "";
+
+  notactive = "";
+  active = "-active";
+
+
+  parentMethod($event) {
+    this.active = $event.detail;
+  }
 
 
 
@@ -25,12 +30,28 @@ export class Shuffle {
 
   }
 
-  mouseOver() {
-    this.active = "-active";
+  mouseOver(elem) {
+    //this.active = "-active";
+    //console.log(document.getElementById(id).src);
+    //console.log(document.getElementById('shuffle-menu'));
+    //document.getElementById(id.id).src = this.assetPath + id + this.active + this.svgFile;
+    console.log('element id: ' + elem.id);
+    //elem.src = this.assetPath + 'crm' + this.active + this.svgFile;
+    /*var event = new CustomEvent('mouseover', {
+      detail: this.active,
+      bubbles: true
+    });
+
+    this.element.dispatchEvent(event);*/
   }
 
-  mouseOut() {
-    this.active = "";
+  mouseOut(elem) {
+    console.log('element id: ' + elem.id);
+    //elem.src = this.assetPath + 'crm' + this.notactive + this.svgFile;
+    //document.getElementById(id.id).src = this.assetPath + id + this.notactive + this.svgFile;
+    //this.active = "";
   }
+
+
 
 }
