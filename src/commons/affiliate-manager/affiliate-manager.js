a797a113d4758a5ee881147941bdb3361349b00c
player1 <altsop@ttu.ee>
2020-03-25 13:06:25 +0200

    - about is component now

--- src/commons/affiliate-manager/affiliate-manager.js
+++ src/commons/affiliate-manager/affiliate-manager.js
@@ -1,10 +1,15 @@
 export class AffiliateManager {
   constructor(ea) {
     this.sidebarIsOpen = false;
+    this.navigatedTo = "About";
   }
 
   toggleSideBar() {
     this.sidebarIsOpen = !this.sidebarIsOpen;
+  }
+
+  navigateTo = (navigateNext) => {
+    this.navigatedTo = navigateNext;
   }
 
 }
---
+++ src/commons/affiliate-manager/components/about.js
@@ -0,0 +1,4 @@
+export class About {
+  constructor(ea) {
+  }
+}
