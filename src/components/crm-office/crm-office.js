903ddc0ff1a5e5480a660e6685ba4c0d9f37a86f
Vladislav <Vladislav.Ogorodnik@ttu.ee>
2020-03-29 21:55:26 +0300

    Add info window

--- src/components/crm-office/crm-office.js
+++ src/components/crm-office/crm-office.js
@@ -1,3 +1,15 @@
 export class CrmOffice {
-
+  constructor() {
+    this.firstName = 'John';
+    this.lastName = 'Corona';
+    this.email = 'cororna@email.com';
+    this.phoneNumber = '+37244534543';
+    this.companyName = 'MINI Company';
+    this.position = 'software Developer';
+    this.companyAddress = 'test';
+    this.addressIndex = 74444;
+  }
+  loadData(id) {
+    return null;
+  }
 }
