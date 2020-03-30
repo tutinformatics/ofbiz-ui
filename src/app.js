b4713c2c98d1eb83cd1a404c030cfa3fcd0f58f0
Viktor <viktor.pavlov@nortal.com>
2020-03-30 16:55:08 +0300

    fix build

--- src/app.js
+++ src/app.js
@@ -6,18 +6,12 @@ export class App {
     config.options.pushState = true;
     config.options.root = '/';
     config.map([
-      {route: '', moduleId: PLATFORM.moduleName('workspace/no-selection'), title: 'Select'},
-      {route: 'workspace/:id', moduleId: PLATFORM.moduleName('workspace/workspace-detail'), name: 'contacts'},
-      {route: 'workspaces', moduleId: PLATFORM.moduleName('workspace/workspaces'), name: 'workspaces'},
-      {route: 'kanban', moduleId: PLATFORM.moduleName('commons/kanban/kanban'), name: 'kanban'},
-      {route: 'object-dist/publisher', moduleId: PLATFORM.moduleName('objektide_levi/publisher/publisher'), name: 'publisher'},
-      {route: 'object-dist', moduleId: PLATFORM.moduleName('objektide_levi/object-dist/object-dist'), name: 'object-dist'},
-      {route: 'kendo-grid',  moduleId: PLATFORM.moduleName('commons/kendo-grid-test/kendo-grid'), name:'grid'},
-      
-      {route: '', redirect: 'tasks'},
-      {route: 'tasks', moduleId: PLATFORM.moduleName('task/task-list'), name: 'tasks'},
-      {route: 'new-task', moduleId: PLATFORM.moduleName('task/task'), name: 'new-task'},
-      {route: 'kanban', moduleId: PLATFORM.moduleName('commons/kanban/kanban'), name: 'kanban'}
+      { route: '', redirect: 'tasks' },
+      { route: 'tasks', moduleId: PLATFORM.moduleName('task/task-list'), name: 'tasks' },
+      { route: 'new-task', moduleId: PLATFORM.moduleName('task/task'), name: 'new-task' },
+      { route: 'kanban', moduleId: PLATFORM.moduleName('commons/kanban/kanban'), name: 'kanban' },
+      { route: 'object-dist/publisher', moduleId: PLATFORM.moduleName('objektide_levi/publisher/publisher'), name: 'publisher' },
+      { route: 'object-dist', moduleId: PLATFORM.moduleName('objektide_levi/object-dist/object-dist'), name: 'object-dist' }
     ]);
     this.router = router;
   }
