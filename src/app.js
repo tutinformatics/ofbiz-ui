0b5e76931cab574b6903b55e8de3cd4f064ddf69
Rihard <riliiv@ttu.ee>
2020-04-05 17:06:15 +0300

    add component

--- src/app.js
+++ src/app.js
@@ -10,8 +10,9 @@ export class App {
       { route: 'tasks', moduleId: PLATFORM.moduleName('task/task-list'), name: 'tasks' },
       { route: 'new-task', moduleId: PLATFORM.moduleName('task/task'), name: 'new-task' },
       { route: 'kanban', moduleId: PLATFORM.moduleName('commons/kanban/kanban'), name: 'kanban' },
+      { route: 'object-dist', moduleId: PLATFORM.moduleName('objektide_levi/object-dist/object-dist'), name: 'object-dist' },
       { route: 'object-dist/publisher', moduleId: PLATFORM.moduleName('objektide_levi/publisher/publisher'), name: 'publisher' },
-      { route: 'object-dist', moduleId: PLATFORM.moduleName('objektide_levi/object-dist/object-dist'), name: 'object-dist' }
+      { route: 'object-dist/query-builder', moduleId: PLATFORM.moduleName('objektide_levi/query-builder/query-builder'), name: 'query-builder' }
     ]);
     this.router = router;
   }
