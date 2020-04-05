d46013877873c59d150ce8d42646ad29cb9f649f
Sirje Maria Lehestik <sirje.maria.001@gmail.com>
2020-04-05 21:55:32 +0300

    Company add via modal working.

--- src/commons/marketdata/marketdata.js
+++ src/commons/marketdata/marketdata.js
@@ -2,8 +2,14 @@ import {HttpClient} from 'aurelia-fetch-client';
 
 export class Marketdata {
   users = [];
-
+  companyName = '';
+  companyCode = '';
+  companyStatus = '';
+  companyAddress = '';
   pageSize = 10;
+  filters = [
+    {value: '', keys: ['company', 'code', 'active', 'address']}
+  ];
 
   bind() {
     let client = new HttpClient();
@@ -11,5 +17,18 @@ export class Marketdata {
     return client.fetch('data.json')
       .then(response => response.json())
       .then(users => this.users = users);
+  }
+
+  submitData() {
+    let company = {
+      company: this.company,
+      code: this.code,
+      active: this.active,
+      address: this.address
+    };
+
+    this.users.unshift(company);
+
+    // ToDO: Make POST HTTP request when back is ready
   }
 }
