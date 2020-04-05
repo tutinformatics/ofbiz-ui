ecf361467e44b9b53abaa7685b5180376d77bdc9
Sirje Maria Lehestik <sirje.maria.001@gmail.com>
2020-04-05 18:38:49 +0300

    Made simple version of table to get the data reading to work

--- src/commons/marketdata/marketdata.js
+++ src/commons/marketdata/marketdata.js
@@ -3,15 +3,13 @@ import {HttpClient} from 'aurelia-fetch-client';
 export class Marketdata {
   users = [];
 
+  pageSize = 10;
+
   bind() {
     let client = new HttpClient();
 
     return client.fetch('data.json')
       .then(response => response.json())
       .then(users => this.users = users);
-  }
-
-  rowSelected($event) {
-    console.log($event.detail.row);
   }
 }
