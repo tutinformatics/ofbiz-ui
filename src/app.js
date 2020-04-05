c3a3c8fbbf609b98af96dd947df04fb4fa776176
Vladislav <Vladislav.Ogorodnik@ttu.ee>
2020-04-05 21:44:42 +0300

    Add personInfoPage

--- src/app.js
+++ src/app.js
@@ -6,12 +6,13 @@ export class App {
     config.title = 'Workspace';
     config.map([
       { route: '', moduleId: PLATFORM.moduleName('workspace/no-selection'),   title: 'Select'},
-      { route: 'workspace/:id',  moduleId: PLATFORM.moduleName('workspace/workspace-detail'), name:'contacts' },
-      { route: 'workspaces',  moduleId: PLATFORM.moduleName('workspace/workspaces'), name:'workspaces' },
-      { route: 'kanban',  moduleId: PLATFORM.moduleName('commons/kanban/kanban'), name:'kanban' },
-      { route: 'kendo-grid',  moduleId: PLATFORM.moduleName('commons/kendo-grid-test/kendo-grid'), name:'grid' },
-      { route: 'kendo-grid',  moduleId: PLATFORM.moduleName('commons/kendo-grid-test/kendo-grid'), name:'grid' },
-      { route: 'crm-office',  moduleId: PLATFORM.moduleName('components/crm-office/crm-office'), name:'crm-office' },
+      { route: 'workspace/:id',  moduleId: PLATFORM.moduleName('workspace/workspace-detail'), name: 'contacts' },
+      { route: 'workspaces',  moduleId: PLATFORM.moduleName('workspace/workspaces'), name: 'workspaces' },
+      { route: 'kanban',  moduleId: PLATFORM.moduleName('commons/kanban/kanban'), name: 'kanban' },
+      { route: 'kendo-grid',  moduleId: PLATFORM.moduleName('commons/kendo-grid-test/kendo-grid'), name: 'grid' },
+      { route: 'kendo-grid',  moduleId: PLATFORM.moduleName('commons/kendo-grid-test/kendo-grid'), name: 'grid' },
+      // { route: 'crm-office',  moduleId: PLATFORM.moduleName('components/crm-office/customerInfoPage/crm-office'), name: 'crm-office' },
+      { route: 'crm-office',  moduleId: PLATFORM.moduleName('components/crm-office/customerInfoPage/customerInfoPage'), name: 'customerInfoPage' },
     ]);
 
     this.router = router;
---
+++ src/components/crm-office/customerInfoPage/customerInfoPage.js
@@ -0,0 +1,12 @@
+export class CustomerInfoPage {
+  constructor() {
+    this.firstName = 'John';
+    this.lastName = 'Corona';
+    this.email = 'cororna@email.com';
+    this.phoneNumber = '+37244534543';
+    this.companyName = 'MINI Company';
+    this.position = 'software Developer';
+    this.companyAddress = 'test';
+    this.addressIndex = 74444;
+  }
+}
