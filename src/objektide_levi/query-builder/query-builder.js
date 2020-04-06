cc113857cfeb58de857d7d97ceeb3b37fd451d47
Raul <raaltm@ttu.ee>
2020-04-06 18:47:09 +0300

    Query builder is working finally

--- src/objektide_levi/query-builder/query-builder.js
+++ src/objektide_levi/query-builder/query-builder.js
@@ -1,3 +1,20 @@
+import { smartQueryBuilder } from "../../../node_modules/smart-webcomponents-community/source/modules/smart.querybuilder.js";
+import "../../../node_modules/smart-webcomponents-community/source/styles/smart.default.css";
 export class queryBuilder {
-
+  constructor() {
+    Smart('#queryBuilder', class {
+      get properties() {
+        return {
+          allowDrag: true,
+          fields: [
+            { label: 'Id', dataField: 'id', dataType: 'number' },
+            { label: 'Product', dataField: 'productName', dataType: 'string' },
+            { label: 'Unit Price', dataField: 'price', dataType: 'number' },
+            { label: 'Purchased', dataField: 'purchased', dataType: 'datetime' },
+            { label: 'Available', dataField: 'available', dataType: 'boolean' }
+          ]
+        }
+      }
+    });
+  }
 }
