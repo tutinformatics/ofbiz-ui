4720a4e26f1482b597c0825507b597029de3af1e
Rihard <riliiv@ttu.ee>
2020-04-06 21:07:37 +0300

    add get method for query service

--- src/objektide_levi/object-dist/object-dist.js
+++ src/objektide_levi/object-dist/object-dist.js
@@ -1,9 +1,15 @@
 import "smart-webcomponents-community/source/styles/smart.default.css";
 import { smartQueryBuilder } from "../../../node_modules/smart-webcomponents-community/source/modules/smart.querybuilder.js";
+import {HttpClient} from "aurelia-fetch-client";
+
+'aurelia-http-client';
 
 export class ObjectDist {
+
+  queryBuilder;
+
   constructor() {
-    Smart('#queryBuilder', class {
+    this.queryBuilder = Smart('#queryBuilder', class {
       get properties() {
         return {
           allowDrag: true,
@@ -17,12 +23,17 @@ export class ObjectDist {
         }
       }
     });
+    this.generateVariables();
+  }
+
+  generateVariables() {
     this.type = '';
     this.variables = [["Puldiauto", "Mängukaru", "Tuulelohe"], ["Arved", "Kasum", "Käive"], ["Püsiklient", "Tavaklient", "Partner"]];
     this.one = "Puldiauto";
     this.two = "Mängukaru";
     this.three = "Tuulelohe";
   }
+
   changeVariables() {
     if (this.type) {
       this.one = this.variables[this.type][0];
@@ -30,6 +41,32 @@ export class ObjectDist {
       this.three = this.variables[this.type][2];
     }
   }
+
+  fetchData() {
+    let httpClient = new HttpClient();
+    httpClient.configure(config => {
+      config
+        .withBaseUrl('api/')
+        .withDefaults({
+          credentials: 'same-origin',
+          headers: {
+            'Accept': 'application/json',
+            'X-Requested-With': 'Fetch'
+          }
+        })
+        .withInterceptor({
+          request(request) {
+            console.log(`Requesting ${request.method} ${request.url}`);
+            return request;
+          },
+          response(response) {
+            console.log(`Received ${response.status} ${response.url}`);
+            return response;
+          }
+        });
+    });
+  }
+
 
   generateKey() {
     console.log("Key Generated");  // TODO: ADD KEY GENERATION FOR PUBLISHER
