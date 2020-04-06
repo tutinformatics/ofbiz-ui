4a0112a6adcdf03244b5bb4dc641af038419430e
Raul <raaltm@ttu.ee>
2020-04-06 20:58:57 +0300

    Publisher querybuilder

--- src/objektide_levi/publisher/publisher.js
+++ src/objektide_levi/publisher/publisher.js
@@ -1,1 +1,18 @@
-export class Publisher {}
+export class Publisher {
+  constructor() {
+    Smart('#queryBuilder', class {
+      get properties() {
+        return {
+          allowDrag: true,
+          fields: [
+            { label: 'Id', dataField: 'id', dataType: 'number' },
+            { label: 'Product', dataField: 'productName', dataType: 'string' },
+            { label: 'Unit Price', dataField: 'price', dataType: 'number' },
+            { label: 'Purchased', dataField: 'purchased', dataType: 'datetime' },
+            { label: 'Available', dataField: 'available', dataType: 'boolean' }
+          ]
+        }
+      }
+    });
+  }
+}
