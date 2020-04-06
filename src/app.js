f521726ce5f71fd9010e8255f3ac118a4f2c89ec
Rihard <riliiv@ttu.ee>
2020-04-06 20:27:52 +0300

    reorganize components

--- src/app.js
+++ src/app.js
@@ -11,8 +11,7 @@ export class App {
       { route: 'new-task', moduleId: PLATFORM.moduleName('task/task'), name: 'new-task' },
       { route: 'kanban', moduleId: PLATFORM.moduleName('commons/kanban/kanban'), name: 'kanban' },
       { route: 'object-dist', moduleId: PLATFORM.moduleName('objektide_levi/object-dist/object-dist'), name: 'object-dist' },
-      { route: 'object-dist/publisher', moduleId: PLATFORM.moduleName('objektide_levi/publisher/publisher'), name: 'publisher' },
-      { route: 'object-dist/query-builder', moduleId: PLATFORM.moduleName('objektide_levi/query-builder/query-builder'), name: 'query-builder' }
+      { route: 'object-dist/publisher', moduleId: PLATFORM.moduleName('objektide_levi/publisher/publisher'), name: 'publisher' }
     ]);
     this.router = router;
   }
--- src/objektide_levi/object-dist/object-dist.js
+++ src/objektide_levi/object-dist/object-dist.js
@@ -1,5 +1,23 @@
+import "smart-webcomponents-community/source/styles/smart.default.css";
+import "./object-dist.css";
+import { smartQueryBuilder } from "../../../node_modules/smart-webcomponents-community/source/modules/smart.querybuilder.js";
+
 export class ObjectDist {
   constructor() {
+    Smart('#queryBuilder', class {
+      get properties() {
+        return {
+          allowDrag: true,
+          fields: [
+            { label: 'Id', dataField: 'id', dataType: 'number' },
+            { label: 'Product', dataField: 'productName', dataType: 'string' },
+            { label: 'Unit Price', dataField: 'price', dataType: 'number' },
+            { label: 'Purchased', dataField: 'purchased', dataType: 'datetime' },
+            { label: 'Available', dataField: 'available', dataType: 'boolean' }
+          ]
+        }
+      }
+    });
     this.type = '';
     this.variables = [["Puldiauto", "Mängukaru", "Tuulelohe"], ["Arved", "Kasum", "Käive"], ["Püsiklient", "Tavaklient", "Partner"]];
     this.one = "Puldiauto";
