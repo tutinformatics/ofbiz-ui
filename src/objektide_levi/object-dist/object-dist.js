b8118addaee28afb8e0c02b7bd18315d58287a22
Rihard <riliiv@ttu.ee>
2020-04-11 18:36:41 +0300

    move query-builder to its own component

--- src/objektide_levi/object-dist/object-dist.js
+++ src/objektide_levi/object-dist/object-dist.js
@@ -1,28 +1,8 @@
-import "smart-webcomponents-community/source/styles/smart.default.css";
-import { smartQueryBuilder } from "../../../node_modules/smart-webcomponents-community/source/modules/smart.querybuilder.js";
 import {HttpClient} from "aurelia-fetch-client";
-
-'aurelia-http-client';
 
 export class ObjectDist {
 
-  queryBuilder;
-
   constructor() {
-    this.queryBuilder = Smart('#queryBuilder', class {
-      get properties() {
-        return {
-          allowDrag: true,
-          fields: [
-            { label: 'Id', dataField: 'id', dataType: 'number' },
-            { label: 'Product', dataField: 'productName', dataType: 'string' },
-            { label: 'Unit Price', dataField: 'price', dataType: 'number' },
-            { label: 'Purchased', dataField: 'purchased', dataType: 'datetime' },
-            { label: 'Available', dataField: 'available', dataType: 'boolean' }
-          ]
-        }
-      }
-    });
     this.generateVariables();
   }
 
---
+++ src/objektide_levi/object-dist/query-builder/query-builder.js
@@ -0,0 +1,25 @@
+import "smart-webcomponents-community/source/styles/smart.default.css";
+import { smartQueryBuilder } from "../../../../node_modules/smart-webcomponents-community/source/modules/smart.querybuilder.js";
+
+export class QueryBuilder {
+
+  queryBuilder;
+
+  constructor() {
+    this.queryBuilder = Smart('#queryBuilder', class {
+      get properties() {
+        return {
+          allowDrag: true,
+          fields: [
+            { label: 'Id', dataField: 'id', dataType: 'number' },
+            { label: 'Product', dataField: 'productName', dataType: 'string' },
+            { label: 'Unit Price', dataField: 'price', dataType: 'number' },
+            { label: 'Purchased', dataField: 'purchased', dataType: 'datetime' },
+            { label: 'Available', dataField: 'available', dataType: 'boolean' }
+          ]
+        }
+      }
+    });
+    console.log("Query Builder Component Initialized...")
+  }
+}
