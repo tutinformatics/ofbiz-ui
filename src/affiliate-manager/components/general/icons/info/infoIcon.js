fe390e7d19d92e4bc66e00f4f7502d34eb8804dd
Aleksei Tsõpov <aleksei.tsopov@ringit.ee>
2020-04-11 20:14:45 +0300

    - member started

--- src/affiliate-manager/components/general/icons/info/infoIcon.js
+++ src/affiliate-manager/components/general/icons/info/infoIcon.js
@@ -1,4 +1,4 @@
-import {faQuestionCircle} from '@fortawesome/free-regular-svg-icons'
+import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons'
 import "../../../../assets/scss/icon.scss";
 
 export class InfoIcon {
