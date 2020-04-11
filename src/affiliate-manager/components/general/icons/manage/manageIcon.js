65f34508bba7603e3a03d31ceedd02a76c2d3e47
Nikita Ojamäe <niojam@ttu.ee>
2020-04-11 16:22:02 +0300

    Add icons

---
+++ src/affiliate-manager/components/general/icons/manage/manageIcon.js
@@ -0,0 +1,7 @@
+import {faPenSquare} from '@fortawesome/free-solid-svg-icons'
+import "../../../../assets/scss/icon.scss";
+import './manageIcon.scss';
+
+export class manageIcon {
+  icon = faPenSquare;
+}
