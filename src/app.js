beb55873c1f5436229deaa1822be761c402c0ce3
likutt <likutt@ttu.ee>
2020-04-12 13:54:07 +0300

    Put active orders view as landing page

--- src/app.js
+++ src/app.js
@@ -6,14 +6,14 @@ export class App {
     config.options.pushState = true;
     config.options.root = '/';
     config.map([
-      { route: '', redirect: 'order-history' },
+      { route: '', redirect: 'active-orders' },
       { route: 'tasks', moduleId: PLATFORM.moduleName('task/task-list'), name: 'tasks' },
       { route: 'new-task', moduleId: PLATFORM.moduleName('task/task-edit'), name: 'new-task' },
       { route: 'kanban', moduleId: PLATFORM.moduleName('commons/kanban/kanban'), name: 'kanban' },
       { route: 'object-dist/publisher', moduleId: PLATFORM.moduleName('objektide_levi/publisher/publisher'), name: 'publisher' },
       { route: 'object-dist', moduleId: PLATFORM.moduleName('objektide_levi/object-dist/object-dist'), name: 'object-dist' },
-      { route: 'order-history', moduleId: PLATFORM.moduleName('customer_view/order_history'), name: 'order-history', nav: true, title: 'Order history'},
       { route: 'active-orders', moduleId: PLATFORM.moduleName('customer_view/active_orders'), name: 'active-orders', nav: true, title: 'Active orders'},
+      { route: 'order-history', moduleId: PLATFORM.moduleName('customer_view/order_history'), name: 'order-history', nav: true, title: 'Order history'},
       { route: 'active-orders-list', moduleId: PLATFORM.moduleName('customer_view/active_orders_list'), name: 'active-orders-list' },
       { route: 'order-view', moduleId: PLATFORM.moduleName('customer_view/order_view'), name: 'order-view'}
     ]);
