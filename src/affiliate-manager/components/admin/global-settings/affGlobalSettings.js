e2e5133514b034a6d0fdb5291cca1f9e6a926646
Aleksei Tsõpov <aleksei.tsopov@ringit.ee>
2020-04-15 18:53:35 +0300

    - commission configuration is added to admin setting

--- src/affiliate-manager/components/admin/global-settings/affGlobalSettings.js
+++ src/affiliate-manager/components/admin/global-settings/affGlobalSettings.js
@@ -6,11 +6,26 @@ export class affGlobalSettings {
   @bindable paymentFrequency;
   @bindable commissionPercent;
   @bindable multiLevelAff;
+  @bindable userGroup;
   @bindable codeInCookieDuration;
   possibleFrequency = ["Each month", "Each week", "Each year", "Each half-year"];
   possibleMultiLevel = ["available", "not available"];
   userGroups = ["affiliate-partners", "legacy-affiliate-partners"];
   globalSettings;
+  productCategories = [
+    {
+      "category": "Smart Devices",
+      "currentCommission": 0.5,
+    },
+    {
+      "category": "Services",
+      "currentCommission": 1.5,
+    },
+    {
+      "category": "Clothes",
+      "currentCommission": 0.3,
+    },
+  ];
 
 
   saveSettings() {
