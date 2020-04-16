9f9f88956ea6bc7bec9d09474efef78c4e4681aa
Aleksei Tsõpov <aleksei.tsopov@ringit.ee>
2020-04-16 16:44:54 +0300

    - filters added to member top partners

--- src/affiliate-manager/components/member/top-partners/affTopPartners.js
+++ src/affiliate-manager/components/member/top-partners/affTopPartners.js
@@ -1,52 +1,83 @@
 export class AffTopPartners {
 
-  counter = 1;
+  constructor() {
+    this.counter = 1;
+    this.topPartnersOptions = this.getTopPartnersOptions();
+    this.allTopPartnes = this.getAllTopPartners();
+    this.filteredTopPartners = this.allTopPartnes;
+  }
 
-  nextCounterValue() {
+  incrementCounter() {
     return this.counter++;
   }
 
-  topPartnersOptions = [
-    {
-      "key": 'place',
-      "value": 'Place',
-    },
-    {
-      "key": 'email',
-      "value": 'Email',
-    },
-    {
-      "key": 'partner-since',
-      "value": 'Partner since',
-    },
-    {
-      "key": 'commission-total',
-      "value": 'Commission Total',
-    },
-    {
-      "key": 'partners-invited',
-      "value": 'Partners invited',
-    },
-  ];
+  getTopPartnersOptions() {
+    return [
+      {
+        "key": 'place',
+        "value": 'Place',
+      },
+      {
+        "key": 'first-name',
+        "value": 'First Name',
+      },
+      {
+        "key": 'last-name',
+        "value": 'Last Name',
+      },
+      {
+        "key": 'email',
+        "value": 'Email',
+      },
+      {
+        "key": 'partner-since',
+        "value": 'Partner since',
+      },
+      {
+        "key": 'commission-total',
+        "value": 'Commission Total',
+      },
+      {
+        "key": 'partners-invited',
+        "value": 'Partners invited',
+      },
+    ];
+  }
 
-  topPartners = [
-    {
-      "email": "mike@gmail.com",
-      "partner-since": "24/11/2020",
-      "commission-total": "25.90$",
-      "partners-invited": "15",
-    },
-    {
-      "email": "mike@gmail.com",
-      "partner-since": "24/11/2020",
-      "commission-total": "25.90$",
-      "partners-invited": "15",
-    },
-    {
-      "email": "mike@gmail.com",
-      "partner-since": "24/11/2020",
-      "commission-total": "25.90$",
-      "partners-invited": "15",
-    },
-  ]
+  getAllTopPartners() {
+    return [
+      {
+        "place": `${this.incrementCounter()}`,
+        "first-name": "Apa",
+        "last-name": "Karls",
+        "email": "mike@gmail.com",
+        "partner-since": "24/11/2020",
+        "commission-total": "25.90$",
+        "partners-invited": "15",
+      },
+      {
+        "place": `${this.incrementCounter()}`,
+        "first-name": "Apa",
+        "last-name": "Karls",
+        "email": "mike@gmail.com",
+        "partner-since": "24/11/2020",
+        "commission-total": "25.90$",
+        "partners-invited": "15",
+      },
+      {
+        "place": `${this.incrementCounter()}`,
+        "first-name": "Apa",
+        "last-name": "Karls",
+        "email": "mike@gmail.com",
+        "partner-since": "24/11/2020",
+        "commission-total": "25.90$",
+        "partners-invited": "15",
+      },
+    ]
+  }
+
+  setFilteredTopPartners(filteredTopPartners) {
+    this.filteredTopPartners = filteredTopPartners;
+  }
+
 }
