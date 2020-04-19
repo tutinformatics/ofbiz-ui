ae006f9b22150160d3fdc8f7638dd139ce84cc73
karljoosepkarp <karljk@ttu.ee>
2020-04-19 17:39:14 +0300

    Revert "add employment fields for testing"
    This reverts commit f303694f

--- src/objektide_levi/object-dist/query-builder/query-builder.js
+++ src/objektide_levi/object-dist/query-builder/query-builder.js
@@ -1,32 +1,35 @@
-import 'smart-webcomponents-community/source/styles/smart.default.css';
-import './query-builder.css';
+import "smart-webcomponents-community/source/styles/smart.default.css";
+import "./query-builder.css";
+import { smartQueryBuilder } from "../../../../node_modules/smart-webcomponents-community/source/modules/smart.querybuilder.js";
+import {HttpClient} from "aurelia-fetch-client";
 
 export class QueryBuilder {
+
   queryBuilder;
   defaultFields = [
-    {label: 'Id', dataField: 'id', dataType: 'number'},
-    {label: 'Product', dataField: 'productName', dataType: 'string'},
-    {label: 'Unit Price', dataField: 'price', dataType: 'number'},
-    {label: 'Purchased', dataField: 'purchased', dataType: 'datetime'},
-    {label: 'Available', dataField: 'available', dataType: 'boolean'}
+    { label: 'Id', dataField: 'id', dataType: 'number' },
+    { label: 'Product', dataField: 'productName', dataType: 'string' },
+    { label: 'Unit Price', dataField: 'price', dataType: 'number' },
+    { label: 'Purchased', dataField: 'purchased', dataType: 'datetime' },
+    { label: 'Available', dataField: 'available', dataType: 'boolean' }
   ]
   customFields = [
-    {label: 'Sender ID', dataField: 'partyIdFrom', dataType: 'number'},
-    {label: 'Due Date', dataField: 'dueDate', dataType: 'datetime'},
-    {label: 'Description', dataField: 'description', dataType: 'string'}
+    { label: 'Sender ID', dataField: 'partyIdFrom', dataType: 'number' },
+    { label: 'Due Date', dataField: 'dueDate', dataType: 'datetime' },
+    { label: 'Description', dataField: 'description', dataType: 'string' },
   ]
 
   constructor() {
-    // eslint-disable-next-line new-cap
     this.queryBuilder = Smart('#queryBuilder', class {
       get properties() {
         return {
           allowDrag: true,
           fields: [
-            {label: 'Party ID', dataField: 'partyIdFrom', dataType: 'number'},
-            {label: 'Role type ID', dataField: 'roleTypeIdTo', dataType: 'string'},
+            { label: 'Sender ID', dataField: 'partyIdFrom', dataType: 'number' },
+            { label: 'Due Date', dataField: 'dueDate', dataType: 'datetime' },
+            { label: 'Description', dataField: 'description', dataType: 'string' },
           ]
-        };
+        }
       }
     });
   }
