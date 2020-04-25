34085c43d746e372f02650a3e31ca1b3241fe296
Vladislav <Vladislav.Ogorodnik@ttu.ee>
2020-04-25 21:37:02 +0300

    FIX fetch problems

--- src/commons/util/HttpClientCRM.js
+++ src/commons/util/HttpClientCRM.js
@@ -6,15 +6,25 @@ export class HttpClientCRM{
   constructor(http){
     this.http = http;
 
-    this.baseUrl = 'http://localhost:8443/api/entities';
+    this.baseUrl = 'https://localhost:8443/api/entities';
 
     this.http.configure(config => {
       config.withBaseUrl(this.baseUrl);
       config.withInterceptor({
         request(request) {
-          request.headers.add("Authorization", "Basic " + btoa(username + ":" + password));
-          ea.publish('http-request', request);
           return request;
+        },
+        requestError(error) {
+          // ea.publish('http-request-error', error);
+          throw error;
+        },
+        response(response) {
+          // ea.publish('http-response', response);
+          return response;
+        },
+        responseError(error) {
+          // ea.publish('http-response-error', error);
+          throw error;
         }
       })
     });
