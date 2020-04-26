a19cabe4ae01d424d6b634464f1fbbe6f8876d57
Artur Kerb <arkerb@taltech.ee>
2020-04-26 19:44:23 +0300

    Added filter to project view.

--- src/project/components/project-list.js
+++ src/project/components/project-list.js
@@ -1,9 +1,15 @@
 import { inject } from 'aurelia-dependency-injection';
 import { Router } from 'aurelia-router';
+import 'bootstrap';
+import {Filter} from "../../commons/filter/filter";
 import { ProjectService } from '../services/project-service';
 
-@inject(Router, ProjectService)
+
+@inject(Router, ProjectService, Filter)
 export class ProjectList {
+
+  filter;
+
   datasource = {
     transport: {
       read: (options) => {
@@ -13,12 +19,32 @@ export class ProjectList {
     }
   };
 
-  constructor(router, projectService) {
+  constructor(router, projectService, filter) {
     this.router = router;
     this.projectService = projectService;
+    this.filter = filter;
   }
 
   handleAddProject() {
     this.router.navigate('new-project');
+  }
+  filterList(){
+    const filterComponents = document.querySelectorAll('smart-query-builder');
+    const filterComponent = filterComponents[0];
+    const filterValues = filterComponent.value;
+    const filters = [];
+    for (let i = 0; i < filterValues.length; i++) {
+      if (typeof filterValues[i] == "object") {
+        let filter = {};
+        for (let j = 0; j < filterValues[i].length; j++) {
+          const data = filterValues[i][j];
+          if (typeof data == "object") {
+            filter[data[0]] = [data[2]];
+          }
+        }
+        filters.push(filter)
+      }
+    }
+    console.log(filters);
   }
 }
