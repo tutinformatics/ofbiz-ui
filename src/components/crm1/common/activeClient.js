a9d11c556a1355bde0ebaccb8775e5ae02c8997f
mabron <mabron@ttu.ee>
2020-04-27 18:20:17 +0300

    Refactoring

--- src/components/crm1/common/activeClient.js
+++ src/components/crm1/common/activeClient.js
@@ -1,3 +1,19 @@
+import {clientQuickActionElement} from './complexViewClient/clientQuickActionElement';
+import {clientContactOptionElement} from './complexViewClient/clientContactOptionElement';
+
 export class ActiveClient {
+  constructor() {
+    this.quickActionOptions = [new clientQuickActionElement("Call"),
+      new clientQuickActionElement("Invoices"),
+      new clientQuickActionElement("Notes"),
+      new clientQuickActionElement("Edit"),
+      new clientQuickActionElement("Add")];
+
+    this.contactOptions = [new clientContactOptionElement("Call"),
+      new clientContactOptionElement("Email"),
+      new clientContactOptionElement("Meeting")];
+  }
+
 
 }
+
