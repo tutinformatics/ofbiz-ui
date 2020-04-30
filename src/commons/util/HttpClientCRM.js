a0cd2320fafdef8be813fbfd4ae69575365c4eb8
Vladislav <Vladislav.Ogorodnik@ttu.ee>
2020-04-30 22:00:54 +0300

    Add no expiring jwt token

--- src/commons/util/HttpClientCRM.js
+++ src/commons/util/HttpClientCRM.js
@@ -9,7 +9,8 @@ export class HttpClientCRM{
     this.baseUrl = 'https://localhost:8443/api/generic/v1';
     // token is only valid for 30min
     // TODO: change for production solution
-    this.token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyTG9naW5JZCI6IkNybXRpaW0iLCJpc3MiOiJBcGFjaGVPRkJpeiIsImV4cCI6MTU4ODA5NjkyOCwiaWF0IjoxNTg4MDk1MTI4fQ.C-Fmrr0iL1rBqTrSHwDSN-LP76zFMCmtMoTxbDv9XpE05ZwCzYGS__-yjiJwiHiTGMNhpKQghKW550vq4ovImA';
+    // Live long token
+    this.token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ1c2VyTG9naW5JZCI6ImFkbWluIiwiaXNzIjoiQXBhY2hlT0ZCaXoiLCJleHAiOjE1ODc4NDI5OTk5OTk3MTgsImlhdCI6MTU4Nzg0MDkxOH0.3hZCbPuEoqQOUTYws1UtPToVuCZrQfaAVYkZIkPvAVd3m1cN-scUpIYErZFGTmMMfYHTEoMlbNlTG5l2GfkDVg';
     this.http.configure(config => {
       config
         .withBaseUrl(this.baseUrl)
