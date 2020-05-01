9e87f9d4ccbd4279b9a04dfa3ef2108ab812cdb8
Vladislav <Vladislav.Ogorodnik@ttu.ee>
2020-05-01 23:33:29 +0300

    Refactor addcontact popup

--- src/components/crm1/common/addClientPopUp.js
+++ src/components/crm1/common/addClientPopUp.js
@@ -1,11 +1,19 @@
-// export class ClientPopUp {
-//   test = 'test';
-//   constructor() {
-//     console.log('const')
-//   }
-// }
+import {EventAggregator} from 'aurelia-event-aggregator';
+import {HttpClientCRM} from '../../../commons/util/HttpClientCRM';
+import {inject} from 'aurelia-dependency-injection';
+
+@inject(EventAggregator, HttpClientCRM)
 export class AddClientPopUp {
-  test = 'THIS MESSAGE IS HERE FOR TESTING PURPOSES';
+  // test = 'THIS MESSAGE IS HERE FOR TESTING PURPOSES';
+
+  constructor(ea, http) {
+    this.ea = ea;
+    this.http = http.http;
+  }
+
+  addContact(contact) {
+    console.info(contact)
+  }
 }
 
 
