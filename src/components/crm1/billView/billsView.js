bbb330d6d732daa38ea20cd6745a0cc34114358d
Alexander <sawka1997@gmail.com>
2020-05-02 19:14:24 +0300

    Added new Views, incomings from back end made a little bit prettier

--- src/components/crm1/billView/billsView.js
+++ src/components/crm1/billView/billsView.js
@@ -1,10 +1,68 @@
-// import {EventAggregator} from 'aurelia-event-aggregator';
-// import {HttpClientCRM} from '../../../commons/util/HttpClientCRM';
-// import {inject} from 'aurelia-dependency-injection';
-// import {Router} from 'aurelia-router';
+import {EventAggregator} from 'aurelia-event-aggregator';
+import {HttpClientCRM} from '../../../commons/util/HttpClientCRM';
+import {json} from 'aurelia-fetch-client';
+import {inject} from 'aurelia-dependency-injection';
+import {Router} from 'aurelia-router';
+import {getDate} from '../../../commons/util/dateConverter';
+import {Bill} from '../models/bill';
 
-
+@inject(EventAggregator, HttpClientCRM, Router)
 export class billsView {
+
+
+  constructor(ea, http, router) {
+    this.ea = ea;
+    this.http = http.http;
+    this.router = router;
+    this.bills = [];
+
+  }
+
+  async attached() {
+    await this.getAllBills();
+
+  }
+
+
+
+  async getAllBills() {
+    let response = await this.http.fetch('/entityquery/InvoiceExport', {
+      method: 'post',
+      body: json({
+        "fieldList": [
+          "partyIdFrom",
+          "partyIdTrans",
+          "amount",
+          "quantity",
+          "invoiceId",
+          "itemDescription",
+          "invoiceTypeId",
+          "invoiceDate"
+        ]
+      })
+    })
+      .then(response => response.json())
+      .catch(() => {
+        alert('Error fetching clients!');
+      });
+
+
+    for (let i = 0; i < response.length; i++) {
+      let invoicedate = getDate(response[i].invoiceDate);
+        let bill = new Bill(
+        response[i].partyIdFrom,
+          invoicedate,
+        response[i].partyIdTrans,
+        response[i].amount,
+        response[i].quantity,
+        response[i].invoiceId,
+        response[i].itemDescription,
+        response[i].invoiceTypeId
+      );
+
+      this.bills.push(bill);
+    }
+  }
 
 
 }
--- src/components/crm1/orderView/ordersView.js
+++ src/components/crm1/orderView/ordersView.js
@@ -1,10 +1,96 @@
-// import {EventAggregator} from 'aurelia-event-aggregator';
-// import {HttpClientCRM} from '../../../commons/util/HttpClientCRM';
-// import {inject} from 'aurelia-dependency-injection';
-// import {Router} from 'aurelia-router';
+import {EventAggregator} from 'aurelia-event-aggregator';
+import {HttpClientCRM} from '../../../commons/util/HttpClientCRM';
+import {json} from 'aurelia-fetch-client';
 
+import {inject} from 'aurelia-dependency-injection';
+import {Order} from '../models/order';
+import {Router} from 'aurelia-router';
+import {getDate} from '../../../commons/util/dateConverter';
 
+@inject(EventAggregator, HttpClientCRM, Router)
 export class ordersView {
+
+  // phone = [ 'Phone'];
+  // mail = [ 'Email'];
+  // pageSize = 10;
+  //
+  // selectedPhone = [];
+  // selectedEmail = [];
+
+
+  constructor(ea, http, router) {
+    this.ea = ea;
+    this.http = http.http;
+    this.router = router;
+    this.orders = [];
+
+  }
+
+  async attached() {
+    await this.getAllOrders();
+
+  }
+
+
+
+  async getAllOrders() {
+    console.log('here');
+    // await this.login();
+    let response = await this.http.fetch('/entityquery/OrderHeaderItemAndInvRoles', {
+      method: 'post',
+      body: json({
+        "fieldList": [
+          "orderId",
+          "orderDate",
+          "entryDate",
+          "partyId",
+          "webSiteId",
+          "roleTypeId",
+          "grandTotal",
+          "statusId"
+        ]
+      })
+    })
+      .then(response => response.json())
+      .catch(() => {
+        alert('Error fetching clients!');
+      });
+
+
+    for (let i = 0; i < response.length; i++) {
+      let orderDate = getDate(response[i].orderDate);
+      let entryDate = getDate(response[i].entryDate);
+      let order = new Order(
+        response[i].orderId,
+        orderDate,
+        entryDate,
+        response[i].partyId,
+        response[i].webSiteId,
+        response[i].roleTypeId,
+        response[i].grandTotal,
+        response[i].statusId
+      );
+
+      this.orders.push(order);
+    }
+  }
+
+
+
+
+
+  // get isPhone() {
+  //   if(this.selectedPhone.length>0){
+  //     return (this.selectedPhone);
+  //   }
+  //   return false;
+  // }
+  // get isEmail() {
+  //   if(this.selectedEmail.length>0){
+  //     return (this.selectedEmail);
+  //   }
+  //   return false;
+  // }
 
 
 }
