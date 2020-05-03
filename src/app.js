0c7b3f635e19d8ba4db27657b3e68d6dc52122b1
Siret Jorro <siretjorro@gmail.com>
2020-05-03 14:53:26 +0300

    merge app.js from master

--- src/app.js
+++ src/app.js
@@ -6,16 +6,16 @@ export class App {
     config.options.pushState = true;
     config.options.root = '/';
     config.map([
-      { route: '', redirect: 'crm/opportunities' },
+      { route: '', redirect: 'tasks' },
       { route: 'tasks', moduleId: PLATFORM.moduleName('task/task-list'), name: 'tasks' },
       { route: 'new-task', moduleId: PLATFORM.moduleName('task/task-edit'), name: 'new-task' },
       { route: 'kanban', moduleId: PLATFORM.moduleName('commons/kanban/kanban'), name: 'kanban' },
       { route: 'object-dist/publisher', moduleId: PLATFORM.moduleName('objektide_levi/publisher/publisher'), name: 'publisher' },
       { route: 'object-dist', moduleId: PLATFORM.moduleName('objektide_levi/object-dist/object-dist'), name: 'object-dist' },
       { route: 'crm/agents', moduleId: PLATFORM.moduleName('crm/agents/agents'), name: 'agents' },
+      { route: 'crm/pipeline', moduleId: PLATFORM.moduleName('./crm/pipeline/pipeline'), name: 'pipeline' },
       { route: 'crm/opportunities', moduleId: PLATFORM.moduleName('./crm/opportunities/opportunities'), name: 'opportunities' },
     ]);
-    config.mapUnknownRoutes('not-found');
     this.router = router;
   }
 }
