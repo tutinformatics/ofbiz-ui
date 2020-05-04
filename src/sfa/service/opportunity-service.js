1426743e264e84c9ba8f12d1411723e8258139a2
Siret Jorro <siretjorro@gmail.com>
2020-05-04 18:26:13 +0300

    change baseurl

--- src/sfa/service/opportunity-service.js
+++ src/sfa/service/opportunity-service.js
@@ -3,7 +3,7 @@ import { inject } from 'aurelia-framework';
 
 @inject(HttpClient)
 export class OpportunityService {
-  baseUrl = 'https://sometotallyrandomapplicationonarandomurl.com:8443/api/api/generic/v1/';
+  baseUrl = '/api/generic/v1/';
 
   constructor(httpClient) {
     this.client = httpClient;
