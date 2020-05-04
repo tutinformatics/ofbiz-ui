8fa884459c210428a067e7dcf0274524b3647281
Siret Jorro <siretjorro@gmail.com>
2020-05-04 17:31:20 +0300

    add quotes view

---
+++ src/sfa/service/quote-service.js
@@ -0,0 +1,36 @@
+import { HttpClient, json } from 'aurelia-fetch-client';
+import { inject } from 'aurelia-framework';
+
+@inject(HttpClient)
+export class QuoteService {
+  baseUrl = '/api/generic/v1/';
+
+  constructor(httpClient) {
+    this.client = httpClient;
+  }
+
+  getQuotes() {
+    return this.client
+      .fetch(`${this.baseUrl}entities/Quote`)
+      .then(response => response.json())
+      .catch(reason => {
+        console.error(reason);
+        return [];
+      });
+  }
+
+  createNewQuote(quote) {
+    this.client
+      .fetch(`${this.baseUrl}entities/Quote`, {
+        method: 'post',
+        body: json(quote)
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(reason => {
+        // do something useful here
+        console.error(reason);
+      });
+  }
+}
+
