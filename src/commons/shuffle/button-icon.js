f1597d4ce5a0a835b993498f6538b86d661b5e4b
aroosi <aroosi@ttu.ee>
2020-05-07 12:23:17 +0300

    Add fixes, now works

--- src/commons/shuffle/button-icon.js
+++ src/commons/shuffle/button-icon.js
@@ -9,22 +9,25 @@ export class ButtonIcon {
   svgFile = '.svg';
   active = '';
   colors = 'gray';
+  bgColors = 'white';
   faIcon;
 
   constructor() {
-    this.element = {
-      active: ''
-    };
+    // this.element = {
+    //   active: ''
+    // };
   }
 
   mouseOver() {
-    this.colors = 'blue';
-    this.element.active = '-active';
+    this.colors = '#1555bd';
+    this.bgColors = '#c7d7f2';
+    // this.element.active = '-active';
   }
 
   mouseOut() {
     this.colors = 'gray';
-    this.element.active = '';
+    this.bgColors = 'white';
+    // this.element.active = '';
   }
 
   elemNameChanged() {
@@ -35,20 +38,28 @@ export class ButtonIcon {
     switch (this.elemName){
       case "project":
         this.faIcon = faListAlt;
+        break;
       case "crm":
         this.faIcon = faFax;
+        break;
       case "accounting":
         this.faIcon = faCalculator;
+        break;
       case "contacts":
         this.faIcon = faAddressBook;
+        break;
       case "manufacturing":
         this.faIcon = faIndustry;
+        break;
       case "sfa":
         this.faIcon = faMagic;
+        break;
       case "marketing":
         this.faIcon = faPoll;
+        break;
       case "invoicing":
         this.faIcon = faFileInvoice;
+        break;
     }
   }
 }
