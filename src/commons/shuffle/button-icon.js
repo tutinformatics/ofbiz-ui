09c4d3e0efb28f78d4c9f5e72a589839f8d8397e
aroosi <aroosi@ttu.ee>
2020-05-07 12:01:07 +0300

    Add changes to apps menu

--- src/commons/shuffle/button-icon.js
+++ src/commons/shuffle/button-icon.js
@@ -1,4 +1,5 @@
 import { customElement, bindable } from 'aurelia-framework';
+import { faCalendarAlt, faListAlt, faAddressBook, faIndustry, faFileInvoice, faPoll, faMagic, faFax, faCalculator } from '@fortawesome/free-solid-svg-icons';
 
 @customElement('button-icon')
 export class ButtonIcon {
@@ -7,18 +8,48 @@ export class ButtonIcon {
   assetPath = '/icons/';
   svgFile = '.svg';
   active = '';
+  colors = 'gray';
+  faIcon;
 
-  constructor() {
+  constructor(elemName) {
     this.element = {
       active: ''
     };
+    this.iconController(elemName);
   }
 
   mouseOver() {
+    this.colors = 'blue';
     this.element.active = '-active';
   }
 
   mouseOut() {
+    this.colors = 'gray';
     this.element.active = '';
+  }
+
+  elemNameChanged() {
+    this.iconController();
+  }
+
+  iconController() {
+    switch (this.elemName){
+      case "project":
+        this.faIcon = faListAlt;
+      case "crm":
+        this.faIcon = faFax;
+      case "accounting":
+        this.faIcon = faCalculator;
+      case "contacts":
+        this.faIcon = faAddressBook;
+      case "manufacturing":
+        this.faIcon = faIndustry;
+      case "sfa":
+        this.faIcon = faMagic;
+      case "marketing":
+        this.faIcon = faPoll;
+      case "invoicing":
+        this.faIcon = faFileInvoice;
+    }
   }
 }
