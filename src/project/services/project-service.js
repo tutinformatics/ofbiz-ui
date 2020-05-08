81d3401d582d903c746af345e0bcbbe931cce88a
Viktor <viktor.pavlov@nortal.com>
2020-05-08 19:44:51 +0300

    get project list from BE for dropdown

--- src/project/services/project-service.js
+++ src/project/services/project-service.js
@@ -1,9 +1,10 @@
 import { inject } from 'aurelia-dependency-injection';
 import { HttpClient, json } from 'aurelia-fetch-client';
+import { SearchUtils } from '../../commons/util/search-utils';
 
 @inject(HttpClient)
 export class ProjectService {
-  baseUrl = 'api/generic/v1/services';
+  baseUrl = 'api/generic/v1';
 
   constructor(httpClient) {
     this.httpClient = httpClient;
@@ -11,7 +12,7 @@ export class ProjectService {
 
   getProjectList() {
     return this.httpClient
-      .fetch(`${this.baseUrl}/getAllProjectList`, {
+      .fetch(`${this.baseUrl}/services/getAllProjectList`, {
         method: 'post',
         body: json({})
       })
@@ -27,7 +28,7 @@ export class ProjectService {
     const body = json(project);
 
     return this.httpClient
-      .fetch(`${this.baseUrl}/createProject`, {
+      .fetch(`${this.baseUrl}/services/createProject`, {
         method: 'post',
         body: body
       })
@@ -35,5 +36,19 @@ export class ProjectService {
         /* eslint no-console: ["error", { allow: ["error"] }] */
         console.error(error);
       }); // TODO: improve error handling
+  }
+
+  getProjectPhaseList(params) {
+    const query = SearchUtils.appendQueryParams(params);
+
+    return this.httpClient
+      .fetch(`${this.baseUrl}/entities/ProjectPartyAndPhase?${query}`)
+      .then((response) => {
+        if (!response.ok) {
+          // TODO: improve error handling
+          throw new Error('An error occured while fetching project phases');
+        }
+        return response.json();
+      });
   }
 }
