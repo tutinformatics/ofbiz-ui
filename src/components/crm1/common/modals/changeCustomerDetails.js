2af7ae8c6494a8d86978907455f17d9a6a58b356
Alexander <sawka1997@gmail.com>
2020-05-10 19:54:31 +0300

    Bug fix

--- src/components/crm1/common/modals/changeCustomerDetails.js
+++ src/components/crm1/common/modals/changeCustomerDetails.js
@@ -1,11 +1,23 @@
-export class ChangeCustomerDetails {
-  constructor() {
-    this.firstName = "Billy"
-    this.lastName = "Herrington"
-    this.companyName = "Company";
-    this.phoneNumber = "37212456789"
-    this.email = "billy-billy@gmail.com"
-    this.position = "CEO"
-  }
 
+import {inject} from "aurelia-dependency-injection";
+import {EventAggregator} from "aurelia-event-aggregator";
+import {Router} from "aurelia-router";
+
+@inject(EventAggregator,  Router)
+export class ChangeCustomerDetails {
+  constructor(ea, http, router) {
+    this.ea = ea;
+
+
+
+    ea.subscribe("contactChosen", payload => {
+      this.chosenContact = payload
+      this.firstName = this.chosenContact.firstName;
+      this.lastName = this.chosenContact.lastName;
+      this.companyName = this.chosenContact.companyName;
+      this.positionType = this.chosenContact.roleTypeId;
+    });
+
+  }
 }
+
