4edc80013f6100e6f2dad603359f0beaf0a2c423
Vladislav <Vladislav.Ogorodnik@ttu.ee>
2020-05-10 21:56:05 +0300

    Add client update

--- src/components/crm1/common/modals/changeCustomerDetails.js
+++ src/components/crm1/common/modals/changeCustomerDetails.js
@@ -1,23 +1,45 @@
-
 import {inject} from "aurelia-dependency-injection";
 import {EventAggregator} from "aurelia-event-aggregator";
 import {Router} from "aurelia-router";
+import {HttpClientCRM} from '../../../../commons/util/HttpClientCRM';
+import {json} from 'aurelia-fetch-client';
+import {readErrorMsg} from '../../utils/alertHandling';
+import * as toastr from 'toastr';
+import {alertConfig} from '../../config/alertConf';
 
-@inject(EventAggregator,  Router)
+@inject(EventAggregator, HttpClientCRM, Router)
 export class ChangeCustomerDetails {
   constructor(ea, http, router) {
+    this.http = http.http;
     this.ea = ea;
-
-
-
     ea.subscribe("contactChosen", payload => {
       this.chosenContact = payload
       this.firstName = this.chosenContact.firstName;
       this.lastName = this.chosenContact.lastName;
       this.companyName = this.chosenContact.companyName;
-      this.positionType = this.chosenContact.roleTypeId;
+      this.positionType = this.chosenContact.position;
+      this.partyId = this.chosenContact.partyId
     });
+  }
 
+  async updatePerson(firstName, lastName) {
+    let response = await this.http.fetch('/services/updatePerson', {
+      method: 'post',
+      body: json({
+        "firstName": firstName,
+        "lastName": lastName,
+        "partyId": this.partyId,
+        "login.username": "admin",
+        "login.password": "ofbiz"
+      })
+    })
+      .then(response => response.json())
+
+    if (response.responseMessage === "error") {
+      let errMsg = readErrorMsg(response);
+      toastr.error(errMsg, "", alertConfig);
+      return null;
+    }
   }
 }
 
