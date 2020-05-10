5138bfefba4d2725ec9df10bbb2b2ac1e659ecd5
Viktor <viktor.pavlov@nortal.com>
2020-05-10 09:54:48 +0300

    fix buttons

--- src/project/components/project-list.js
+++ src/project/components/project-list.js
@@ -1,6 +1,7 @@
 import { inject } from 'aurelia-dependency-injection';
 import { Router } from 'aurelia-router';
 import { ProjectService } from '../services/project-service';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @inject(Router, ProjectService)
 export class ProjectList {
@@ -16,6 +17,7 @@ export class ProjectList {
   constructor(router, projectService) {
     this.router = router;
     this.projectService = projectService;
+    this.faPlus = faPlus;
   }
 
   handleAddProject() {
