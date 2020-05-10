299a031b13b06d48cb05c7c402e7f8e81d18159c
Viktor <viktor.pavlov@nortal.com>
2020-05-10 16:30:56 +0300

    added vaadin vol.2

--- src/commons/services/navigation-service.js
+++ src/commons/services/navigation-service.js
@@ -2,82 +2,96 @@ import * as _ from 'lodash';
 import {PLATFORM} from 'aurelia-pal';
 
 const routes = {
-  'project': [
+  project: [
     {
-      route: '',
-      moduleId: PLATFORM.moduleName('project/components/project-list'),
-      name: 'project',
+      route: "",
+      moduleId: PLATFORM.moduleName("project/components/project-list"),
+      name: "project",
       nav: true,
-      title: 'Projects'
+      title: "Projects",
     },
     {
-      route: 'my-tasks',
-      moduleId: PLATFORM.moduleName('project/task/components/task-list'),
-      name: 'my-tasks',
+      route: "my-tasks",
+      moduleId: PLATFORM.moduleName("project/task/components/task-list"),
+      name: "my-tasks",
       nav: true,
-      title: 'My Tasks'
+      title: "My Tasks",
     },
     {
-      route: 'project/:id',
-      moduleId: PLATFORM.moduleName('project/components/project-view'),
-      name: 'project-view'
+      route: "new-task",
+      moduleId: PLATFORM.moduleName("project/task/components/task-edit"),
+      name: "new-task",
+      title: "Create Task",
     },
     {
-      route: 'new-project',
-      moduleId: PLATFORM.moduleName('project/components/project-edit'),
-      name: 'new-project',
-      title: 'Create Project'
+      route: "project/:id",
+      moduleId: PLATFORM.moduleName("project/components/project-view"),
+      name: "project-view",
     },
     {
-      route: 'resources',
-      moduleId: PLATFORM.moduleName('project/resource/components/resource-list'),
-      name: 'resource',
-      title: 'Resources',
-      nav: true
+      route: "new-project",
+      moduleId: PLATFORM.moduleName("project/components/project-edit"),
+      name: "new-project",
+      title: "Create Project",
     },
     {
-      route: 'new-resource',
-      moduleId: PLATFORM.moduleName('project/resource/components/resource-edit'),
-      name: 'new-resource'
+      route: "resources",
+      moduleId: PLATFORM.moduleName(
+        "project/resource/components/resource-list"
+      ),
+      name: "resource",
+      title: "Resources",
+      nav: true,
     },
     {
-      route: 'timesheets',
-      moduleId: PLATFORM.moduleName('project/timesheet/components/timesheet-list'),
-      title: 'Timesheets',
-      name: 'timesheets',
-      nav: true
+      route: "new-resource",
+      moduleId: PLATFORM.moduleName(
+        "project/resource/components/resource-edit"
+      ),
+      name: "new-resource",
     },
     {
-      route: 'new-timesheet',
-      moduleId: PLATFORM.moduleName('project/timesheet/components/timesheet-edit'),
-      title: 'Create timesheet',
-      name: 'new-timesheet'
-    }
+      route: "timesheets",
+      moduleId: PLATFORM.moduleName(
+        "project/timesheet/components/timesheet-list"
+      ),
+      title: "Timesheets",
+      name: "timesheets",
+      nav: true,
+    },
+    {
+      route: "new-timesheet",
+      moduleId: PLATFORM.moduleName(
+        "project/timesheet/components/timesheet-edit"
+      ),
+      title: "Create timesheet",
+      name: "new-timesheet",
+    },
   ],
-  'sfa': [
-    { route: '', redirect: 'pipeline' },
+  sfa: [
+    { route: "", redirect: "pipeline" },
     {
-      route: 'pipeline',
-      moduleId: PLATFORM.moduleName('crm/components/pipeline/pipeline'),
-      name: 'pipeline',
-      title: 'CRM',
-      nav: true
+      route: "pipeline",
+      moduleId: PLATFORM.moduleName("crm/components/pipeline/pipeline"),
+      name: "pipeline",
+      title: "CRM",
+      nav: true,
     },
     {
-      route: 'listview',
-      moduleId: PLATFORM.moduleName('crm/components/listview/listview'),
-      name: 'listview'
+      route: "listview",
+      moduleId: PLATFORM.moduleName("crm/components/listview/listview"),
+      name: "listview",
     },
     {
-      route: 'cardview',
-      moduleId: PLATFORM.moduleName('crm/components/cardview/cardview'),
-      name: 'cardview'
-    }
+      route: "cardview",
+      moduleId: PLATFORM.moduleName("crm/components/cardview/cardview"),
+      name: "cardview",
+    },
   ],
-  'crm': [], // TODO: add crm child routes here
-  'cms': [], // TODO: add cms child routes here
-  'marketdata': [], // TODO: add marketdata child routes here
-  'object-dist': [] // TODO: add objectdist child routes here
+  crm: [], // TODO: add crm child routes here
+  cms: [], // TODO: add cms child routes here
+  marketdata: [], // TODO: add marketdata child routes here
+  "object-dist": [], // TODO: add objectdist child routes here
 };
 
 export class NavigationService {
