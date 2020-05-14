276dd8f8068b09af1f77b1e0c19efee077f5d61b
mabron <mabron@ttu.ee>
2020-05-14 16:52:37 +0300

    maybe more modals

--- src/components/crm1/common/modals/clientDetailPopUp.js
+++ src/components/crm1/common/modals/clientDetailPopUp.js
@@ -8,11 +8,37 @@ export class ClientDetailPopUp {
   constructor(ea, http) {
     this.ea = ea;
     this.http = http;
+    this.showSaveAndClose = false
+    this.canEdit = false
+
+    this.value = "Edit"
     ea.subscribe("currentClient", payload => {
       console.log(payload)
       this.contact = payload
     })
   }
+
+  edit(contact) {
+    console.log(contact)
+    if (this.value === "Edit") {
+      this.value = "Save"
+      this.showSaveAndClose = true
+      this.canEdit = true
+    } else  {
+      this.value = "Edit"
+      this.showSaveAndClose = false
+    }
+  }
+
+  addContactAndClose(contact) {
+    console.log("sad")
+  }
+
+  resetState() {
+    this.canEdit = false
+  }
+
+
 }
 
 
